efebb498e0c80620e97c56cc8328bf9ab7484825
aryan00-dev <indiastfc@gmail.com>
2026-04-26 20:56:16 +0530

    Delete voice_engine.py

--- voice_engine.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-
-def make_audio(text, filename="audio.mp3"):
-    print("Generating Hindi Voiceover...")
-    tts = gTTS(text=text, lang='hi')
-    tts.save(filename)
-    return filename
-
